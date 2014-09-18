258074d677595cdc0753a439c3f3b935c87a250b
Craig Lager <craig@prezzybox.com>
2014-09-18 20:31:51 +0100

    Init

---
+++ jQuery.better.js
@@ -0,0 +1,27 @@
+$.fn.goAway = function(){
+	return this.addClass("hidden");
+}
+
+$.fn.noWaitComeBack = function(){
+	return this.removeClass("hidden");
+}
+
+$fn.whereAreYou = function(){
+	return this.css('border', 'color: red');
+}
+
+$.nawM8 = function(){
+	$('body').html('brye');
+}
+
+$.fn.uW0tM8 = function(){
+	console.log(this);
+}
+
+$.sup = function(){
+	alert("not much");
+}
+
+$.makeBetter = function(){
+	$('img').attr('src','http://ichef.bbci.co.uk/food/ic/food_1x1_304/chefs/gregg_wallace_1x1.jpg');
+}
