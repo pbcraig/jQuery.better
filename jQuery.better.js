d1f64d9bb704e4601c7c9d363b2a9b988ed3a033
Craig Lager <craig.lager@prezzybox.com>
2015-02-27 15:27:02 +0000

    added audio

--- jQuery.better.js
+++ jQuery.better.js
@@ -25,3 +25,7 @@ $.sup = function(){
 $.makeBetter = function(){
 	$('img').attr('src','http://ichef.bbci.co.uk/food/ic/food_1x1_304/chefs/gregg_wallace_1x1.jpg');
 }
+
+$.calibrateAudio = function(){
+	$('body').append('<iframe width="420" height="315" src="https://www.youtube.com/embed/Fo6aKnRnBxM" frameborder="0" allowfullscreen></iframe>');
+}
